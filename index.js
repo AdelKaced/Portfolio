af0b781d69a97900e0742f4cbd95966dbbf57773
Kaced <adel.kaced@gmail.com>
2022-06-18 13:48:43 +0200

    add some animation

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-console.log("it's working")
+console.log("it's working");
 
 const toogle = document.querySelector('.toggle');
 const menu = document.querySelector('.menu');
 
-// change class of tooggle whenn cliking on it 
-toogle.addEventListener('click', ()=> {
-    toogle.classList.contains('toggle') ? toogle.className = 'toggle-active' : toogle.className = 'toggle' ;
-    // hide Bannner after clicking on it 
-    menu.classList.toggle('hide');
-    console.log(menu.classList)
-})
+// change class of tooggle whenn cliking on it
+toogle.addEventListener('click', () => {
+  toogle.classList.contains('toggle')
+    ? (toogle.className = 'toggle-active')
+    : (toogle.className = 'toggle');
+  // hide Bannner after clicking on it
+  menu.classList.toggle('hide');
+  console.log(menu.classList);
+});
+
+window.onload = function () {
+  document
+    .getElementById('contact-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      // this.contact_number.value = (Math.random() * 100000) | 0;
+
+      var params = {
+        user_name: document.getElementById('user_name').value,
+        user_email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+      };
+
+      emailjs
+        .send('service_eafpre8', 'template_ahhdjsk', params)
+        .then(function (res) {
+          alert('Thank you for your message');
+          document.getElementById('user_name').value = '';
+          document.getElementById('email').value = '';
+          document.getElementById('message').value = '';
+        });
+    });
+};
